d8d529f51991ab5222c306a3ac80c8b574d580a3
Karan Parashar <whyredfire@gmail.com>
2021-08-16 00:02:31 +0530

    ginkgo: inherit common_full_phone - removed arrow dependencies - fix copyright headers
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -24,11 +24,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common Not-So-Pixel stuff
+# Inherit some common PixelOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier
 PRODUCT_NAME := aosp_ginkgo
--- parts/src/org/lineageos/settings/display/KcalUtils.java
+++ parts/src/org/lineageos/settings/display/KcalUtils.java
@@ -1,7 +1,6 @@
 
 /*
- * Copyright (C) 2021 PixelExperience
- *
+ * Copyright (C) 2020 ArrowOS
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at

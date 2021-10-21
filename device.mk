f4b74e2b5946698549ca875cd6c3870dd0f32e0e
Karan Parashar <whyredfire@gmail.com>
2021-10-21 09:42:36 +0530

    ginkgo: build android.hardware.bluetooth@1.1 I/hwservicemanager(482): getTransport: Cannot find entry android.hardware.bluetooth@1.1::IBluetoothHci/default in either framework or device VINTF manifest. I/hwservicemanager(482): Since android.hardware.bluetooth@1.0::IBluetoothHci/default is not registered, trying to start it as a lazy HAL. I/HidlServiceManagement(15065): getService: Trying again for android.hardware.bluetooth@1.0::IBluetoothHci/default...
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbtconfigstore \

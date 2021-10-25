5fa4b1156a5b932402069268efd9400009e6b72b
Karan Parashar <whyredfire@gmail.com>
2021-10-25 12:54:11 +0530

    ginkgo: drop SELINUX_IGNORE_NEVERALLOWS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current

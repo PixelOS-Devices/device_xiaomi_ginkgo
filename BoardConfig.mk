2bf0c05306799b338bd3d4f3f6ae6637e1d9dbc8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-30 18:47:09 +0530

    ginkgo: Adapt sepolicy board flags for A12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,9 +156,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current

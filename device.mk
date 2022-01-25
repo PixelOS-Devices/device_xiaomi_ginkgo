fcda576aeb3535250480840117cbeb7498b4ba51
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    ginkgo: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>

--- device.mk
+++ device.mk
@@ -69,7 +69,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbtconfigstore \

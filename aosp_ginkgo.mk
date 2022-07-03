30d6219cde28043c15112f3faeb556e8a0b91dc2
Karan Parashar <whyredfire@gmail.com>
2022-07-03 15:07:54 +0530

    ginkgo: Drop redundant face unlock flag
     - face unlock is built by default in [1]
    
     [1] https://github.com/PixelOS-Pixelish/vendor_aosp/blob/58ff76216d2bd74fc97945242480e4049ef93f37/config/common.mk#L235
    
    Change-Id: I2250f9cdc096f13ad4c1197151129f0353c0c7ff

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -27,7 +27,6 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 # Inherit some common PixelOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)

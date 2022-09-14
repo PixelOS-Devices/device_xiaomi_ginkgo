23f856783797baf22f1b032070f12acf7c47a8a2
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    ginkgo: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -140,7 +140,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor
 
 # Fingerprint

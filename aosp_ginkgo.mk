306e53cbe8719c6093eb36059bdd624c73ff34e2
Karan Parashar <karan@pixelos.net>
2023-01-12 05:48:25 +0530

    ginkgo: Adapt for PixelOS
    Change-Id: Ia305f0f0fbd7b9fabb30b62ea4adb40d52f78e9e

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2020 ArrowOS
+# Copyright (C) 2023 PixelOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,14 +26,12 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common ArrowOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := arrow_ginkgo
+PRODUCT_NAME := aosp_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-DEVICE_MAINTAINER := Adithya (ghostrider_reborn)

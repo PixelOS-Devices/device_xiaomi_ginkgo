e352cfd673434222195c27e63a6a38117ccc0e0c
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    ginkgo: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    For now this is implicitly built by lineage specific
    libprotobuf-cpp-lite-3.9.1-vendorcompat lib, but in the future this
    might not be the case.
    
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- device.mk
+++ device.mk
@@ -316,6 +316,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QTI VNDK Framework Detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
